3465a622dd845df0f3186e47fd3ce0b051d57381
Fuad Hasan Shihab <85599261+fuadhasanshihab@users.noreply.github.com>
2025-05-10 13:21:26 +0000

    astro sitemap add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
 if ((process.env.ts || "") !== "1") process.exit(1);
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://banglawebfonts.pages.dev',
+  integrations: [sitemap(),tailwind()],
 });
