3efed4e47b149753fda67bbe18ce7411eb78ccd0
Fuad Hasan Shihab <85599261+fuadhasanshihab@users.noreply.github.com>
2025-05-13 07:23:51 +0000

    installed preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from '@astrojs/sitemap';
+import preact from '@astrojs/preact';
 if ((process.env.ts || "") !== "1") process.exit(1);
 export default defineConfig({
   site: 'https://banglawebfonts.pages.dev',
-  integrations: [sitemap(),tailwind()],
+  integrations: [sitemap(),tailwind(),preact()],
 });
