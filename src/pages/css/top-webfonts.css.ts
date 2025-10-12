f599167ae8e52f89e1ecf580b4652a9e7700510d
Fuad Hasan Shihab <85599261+fuadhasanshihab@users.noreply.github.com>
2025-10-12 21:05:41 +0600

    Create top-webfonts.css.ts

---
+++ src/pages/css/top-webfonts.css.ts
@@ -0,0 +1,14 @@
+import fonts from '../../collections/fonts.json';
+
+export function GET() {
+  let cssContent = fonts
+    .slice(0, 4)
+    .map(font => {
+      return `@font-face{font-family:'${font.FontName}';src:url('/fonts/${font.FontPath}/${font.FontPath}-regular.woff2') format('woff2');font-weight:400;font-style:normal;font-display:swap;}`;
+    })
+    .join('');
+
+  return new Response(cssContent, {
+    headers: { 'Content-Type': 'text/css' },
+  });
+}
